366730532283415ba90e528a15fb40cab9744c64
chiro <jztnem@gmail.com>
2024-12-08 22:34:44 +0100

    add new config variables

--- app/config.py
+++ app/config.py
@@ -4,7 +4,8 @@ DB_NAME = "pdb_mirror"
 DB_USER = "admin"
 DB_PASSWORD = "admin"
 
-PDB_URL = ""
+PDB_FTP_URL = "files-versioned.wwpdb.org"
+PDB_VIEW_URL = "https://files-versioned.rcsb.org/pdb_versioned/views/latest/coordinates/mmcif/"
 FETCH_JOB_DAY = "thu"
 
 API_PATH = "/api/v1"
