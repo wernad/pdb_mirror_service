e9e81491cd64577f0e6e0a57709dcb7be5fa055f
chiro <jztnem@gmail.com>
2025-02-18 05:55:59 +0100

    add protein service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,2 +1,3 @@
+from app.services.protein import ProteinService
 from app.services.files import FileService
 from app.services.failed import FailedFetchService
---
+++ app/services/protein.py
@@ -0,0 +1,16 @@
+from sqlmodel import Session
+from app.database.repositories import ProteinRepository
+
+from app.log import logger as log
+
+
+class ProteinService:
+    protein_repository: ProteinRepository
+
+    def __init__(self, db: Session):
+        self.protein_repository = ProteinRepository(db)
+
+    def deprecate_protein(self, protein_id: str):
+        """Deprecated given protein entry."""
+
+        self.protein_repository.update_depricated(protein_id=protein_id, status=False)
