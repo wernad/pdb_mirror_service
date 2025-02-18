804da35468fe906aeaf57f637bcc9423afa94871
chiro <jztnem@gmail.com>
2025-02-18 05:56:54 +0100

    update config variables

--- app/config.py
+++ app/config.py
@@ -4,23 +4,17 @@ DB_NAME = "pdb_mirror"
 DB_USER = "admin"
 DB_PASSWORD = "admin"
 
-PDB_RSYNC_URL = "rsync://rsync-versioned.pdbj.org/ftp_versioned/data"
-PDB_RSYNC_COMMAND = [
-    "rsync",
-    "--dry-run",
-    "--list-only",
-    "-r",
-    "--include",
-    "'*/'",
-    "--include",
-    " '*.cif.gz'",
-    "--exclude",
-    "'*'",
-]
+
+PDB_SEARCH_API_URL = "https://search.rcsb.org/rcsbsearch/v2/query"
+PDB_SEARCH_API_LIMIT = 1000
+
+PDB_DATA_API_URL = "https://data.rcsb.org/graphql"
+
+PDB_FTP_STATUS_URL = "https://files.rcsb.org/pub/pdb/data/status/"
 
 PDB_HTTP_FILE_AGE = 10  # In days.
 PDB_HTTP_TIMEOUT = 5  # Seconds.
-PDB_HTTP_VIEW_URL = "https://files-versioned.rcsb.org/pdb_versioned/views/latest/coordinates/mmcif/"
+PDB_HTTP_FILE_URL = "https://files-versioned.wwpdb.org/pdb_versioned/views/all/coordinates/mmcif/"
 
 CRON_JOB_DAY = 3  # 0-6 (Mon - Sun).
 
