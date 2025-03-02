b813de68992f346f570a49c8fe85c68eb6abe8f7
chiro <jztnem@gmail.com>
2025-03-02 19:13:54 +0100

    update imports

--- app/database/repositories/__init__.py
+++ app/database/repositories/__init__.py
@@ -1,3 +1,4 @@
 from app.database.repositories.files import FileRepository
 from app.database.repositories.protein import ProteinRepository
 from app.database.repositories.failed import FailedFetchRepository
+from app.database.repositories.change import ChangeRepository
