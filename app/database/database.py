e916e73c1aa0424ce2ab29465bb93738a9929b07
chiro <jztnem@gmail.com>
2025-03-07 16:52:23 +0100

    add context manager variant of session creation

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,10 @@
+from contextlib import contextmanager
 from collections.abc import Generator
 from sqlmodel import Session, create_engine, SQLModel
 from app.config import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 from pydantic_core import MultiHostUrl
+
+__all__ = ["get_session", "db_context", "create_db_and_tables"]
 
 DATABASE_URL = str(
     MultiHostUrl.build(
@@ -21,6 +24,9 @@ engine = create_engine(DATABASE_URL, echo=True)
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
+
+db_context = contextmanager(get_session)
 
 
 def create_db_and_tables():
