cdd63d58119ac0916b1acda7ac57efe64ca61b83
chiro <jztnem@gmail.com>
2025-03-07 16:52:48 +0100

    add bulk insert

--- app/services/change.py
+++ app/services/change.py
@@ -1,13 +1,22 @@
 from sqlmodel import Session
 from app.database.repositories import ChangeRepository
 
-from app.database.models import Change
-from app.log import logger as log
+from app.database.models import ChangeInsert
 
 
-# TODO
 class ChangeService:
     change_repository: ChangeRepository
 
     def __init__(self, db: Session):
         self.change_repository = ChangeRepository(db)
+
+    def insert_bulk_added(self, entries: list[ChangeInsert]):
+        """Inserts information about protein entry changes."""
+
+        values = []
+        for entry in entries:
+            values.append(
+                {"protein_id": entry.protein_id, "operation_flag": entry.operation_flag, "timestamp": entry.timestamp}
+            )
+
+        self.change_repository.insert_bulk(entries)
