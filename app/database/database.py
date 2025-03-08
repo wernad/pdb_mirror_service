0902a7cf29e28e2a8245e08ca9a0ff29858c64d5
chiro <jztnem@gmail.com>
2025-03-08 18:16:28 +0100

    add enviroment variables for database

--- app/database/database.py
+++ app/database/database.py
@@ -3,17 +3,18 @@ from collections.abc import Generator
 from sqlmodel import Session, create_engine, SQLModel
 from app.config import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 from pydantic_core import MultiHostUrl
+from os import environ
 
 __all__ = ["get_session", "db_context", "create_db_and_tables"]
 
 DATABASE_URL = str(
     MultiHostUrl.build(
         scheme="postgresql",
-        username=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        path=DB_NAME,
+        username=environ.get("DB_USER", DB_USER),
+        password=environ.get("DB_PASSWORD", DB_PASSWORD),
+        host=environ.get("DB_HOST", DB_HOST),
+        port=environ.get("DB_PORT", DB_PORT),
+        path=environ.get("DB_NAME", DB_NAME),
     )
 )
 
